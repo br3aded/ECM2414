2e23b9f6734f981e934e9e829b84dec5c1b35a53
MatthewWood-Github <matthewwood2002@gmail.com>
2022-11-04 13:09:23 +0000

    Player hand class changes

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,6 @@
 public class Player {
     private static int counter;
     private int id;
-    private int handSize = 4;
-    private Card[] hand = new Card[handSize];
 
     public Player()
     {
@@ -14,25 +12,31 @@ public class Player {
         return id;
     }
 
-    public Card[] getHand()
-    {
-        return hand;
-    }
-
-    private void drawCard()
-    {
-        // Assign previous deck;
-        // hand.add(previousDeck.deQueue());
-    }
-
-    private void pushCard(Card card)
-    {
-        // Assign next deck;
-        // nextDeck.enQueue(hand.pop(card));
-    }
-
     private void checkWin()
     {
         // If win: stop all threads and end game.
+    }
+    
+    public class PlayerHand
+    {
+    	private int handSize = 4;
+        private Card[] hand = new Card[handSize];
+        
+        public Card[] getHand()
+        {
+            return hand;
+        }
+
+        private void drawCard()
+        {
+            // Assign previous deck;
+            // hand.add(previousDeck.deQueue());
+        }
+
+        private void pushCard(Card card)
+        {
+            // Assign next deck;
+            // nextDeck.enQueue(hand.pop(card));
+        }
     }
 }
