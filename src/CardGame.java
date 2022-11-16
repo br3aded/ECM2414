75a1547a33d2c87c0897f32690fbc947f75f19ec
br3aded <kelseyRholdgate@icloud.com>
2022-11-16 15:41:24 +0000

    Adding comments

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,6 +1,6 @@
+//All the imports
 import java.io.BufferedReader;
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.File; 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CardGame {
-	
+	//Creates the ArrayList and some variables used within the threads
 	private volatile static ArrayList<Player> Players = new ArrayList<Player>();
 	private static ArrayList<CardDeck> CardDecks = new ArrayList<CardDeck>();
 	private static ArrayList<Thread> PlayerThreads = new ArrayList<Thread>();
@@ -17,32 +17,46 @@ public class CardGame {
 	private static Integer winner;
 	private static Integer threadsFinished;
 	
-	public static void main(String[] args) throws IOException, InterruptedException {	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		//asks user for the number of player and stores
 		Scanner myScan1 = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("Please enter the number of players:");//Asks for number of players
 	    numberOfPlayers = myScan1.nextInt();  // Read user input
 	    
+	    //generates all the card objects and stores in Cards
 	    ArrayList<Card> Cards = packReader(numberOfPlayers);
 	    myScan1.close();
+	    
+	    //Creates all the CardDeck Objects and stores in CardDecks
 	    generateDecks();
+	    //Creates all the Player Objects and stores within Players , this must be done after generateDecks
 	    generatePlayers();
+	    //Distributes all the Cards to each Player and CardDeck Object
 	    distributeCards(Cards);
+	    //This creates all the Player Threads and stores within PlayerThreads
 	    createThreads();
+	    //This starts all the [layer Threads
 	    startThreads();
 	}
 	
 	private static void createThreads() throws IOException{
-		
+		//Initialised some values that are used within the threads
 		done = false;
 	    threadsFinished = 0;
-		threadsWaiting = 0;
+	    //Creates the create number of threads for the number of players
 		for(int i=0; i<numberOfPlayers ;i++) {
+			//Used to pass each threads it correct Player Value
 			final int passThreadCounter = i;
+			//Initialises the threads
 			Thread thread = new Thread(new Runnable(){
 	            @Override
+	            //Code for each thread
 	            public void run(){
+	            	//Passes the Player Value to the thread
 	            	int threadCounter = passThreadCounter;
+	            	//Creates a new output file for each thread
 		            File outputFile = new File("player" + (threadCounter+1) + "_output.txt" );
+		            //Writes the initial hand to the player output file
 		            try {
 		            	FileWriter writer = new FileWriter(outputFile,true);
 						writer.write("player " + (threadCounter +1) + " inital hand " + Players.get(threadCounter).getHand().displayHand());
@@ -51,29 +65,38 @@ public class CardGame {
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
-		            //does an initial check to see if anyone has won
+		            //does an initial check to see if anyone has won and will prevent the game from starting if this is true
 		            if (Players.get(threadCounter).checkWin() == true && winner == null) {
 		            	winner = threadCounter + 1;
 	                	done = true;
 	                	System.out.println("Player " + (threadCounter+1) +" has won");
                 	}
 		            
+		            //the game is actually played in this while loop
 		            while(!done) { 
 		            	try{
+		            		//the file writer is opened
 		            		FileWriter writer = new FileWriter(outputFile,true);
+		            		//this writes to the player output the actions of a draw and also carries out the player draw from the player class
 				            writer.write("player " + (threadCounter +1) + " draws a " + Players.get(threadCounter).getHand().drawCard().getValue() + " from deck " + Players.get(threadCounter).getLeft().getId());
 				            writer.write("\r\n");
+				            //this writes to the player output the actions of a discard and also carries out the player discard from the player class
 				            writer.write("player " + (threadCounter +1) + " discards a " + Players.get(threadCounter).getHand().pushCard().getValue() + " to deck " + Players.get(threadCounter).getRight().getId());
 				            writer.write("\r\n");
+				            //file writer is closed
 				            writer.close();
 				         } catch (IOException e) {
 				            e.printStackTrace();
 				         } 
+		            	//runs a check after one turn is played if the player has won
 				         if ((Players.get(threadCounter)).checkWin() == true) {
+				        	 //sets done to true so all the threads will end after they finish there current iteration
 				        	 done = true;
+				        	 //creates a value to allow all the output files to display the correct winner
 				             winner = threadCounter + 1;
 				             System.out.println("Player " + (threadCounter+1) +" has won");
 			            }
+				        //put all the threads to sleep for 10 milliseconds to prevent threads from being 1 iteration ahead of the winning thread
 				        try {
 							PlayerThreads.get(threadCounter);
 							Thread.sleep(10);
@@ -83,6 +106,9 @@ public class CardGame {
 						}
 		            }
 		            
+		            //this code executes after a winner has been declared
+		            
+		            //this writes the game finishing outputs to each players output file
 		            try {
 						FileWriter writer = new FileWriter(outputFile,true);
 						writer.write("player " + winner + " has won");
@@ -96,22 +122,24 @@ public class CardGame {
 						e.printStackTrace();
 					}
 		          
+		            //this checks to see if the current thread is the final thread to finish , if so it generates the deck output files
 		            if(threadsFinished == PlayerThreads.size()-1) {
 		            	try {
+		            		//this generates the deck output files
 							deckOutput();
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-		            }else {threadsFinished++ ;}
+		            }else {threadsFinished++ ;} // adds to threadFinished counter if the current thread isn't the last thread to finish
 	            }
 	            
 	        });
-			PlayerThreads.add(thread);
+			PlayerThreads.add(thread);//adds each thread to the PlayerThreads ArrayList
 		}
 	}
 	
 	private static void startThreads() {
+		//uses the PlayerThreads array list to start each thread
 		for(int i=0; i<PlayerThreads.size();i++) {
 			PlayerThreads.get(i).start();
 		}
@@ -119,17 +147,23 @@ public class CardGame {
 		
 	}
 	
-	//need to check for incorrect pack length and incorrect pack name
-	//add if pack length is incorrect it starts it again
 	private static ArrayList<Card> packReader(Integer numberOfPlayers) throws IOException{
-    	
+    	boolean exists = false;
+    	Scanner myScan2 = new Scanner(System.in); // Create a Scanner object
+    	File f = null;
 		//reads file name
-    	Scanner myScan2 = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Please enter the file Name:");
-	    String fileName = myScan2.nextLine();  // Read user input
-	    
-	    //reads file
-	    File f = new File("src/" + fileName);
+    	while(!exists) {
+		    System.out.println("Please enter the file Name:");
+		    String fileName = myScan2.nextLine();  // Read user input
+		    //reads file
+		    f = new File(fileName);
+		    if(f.exists() == true) {
+		    	exists = true;
+		    }else {
+		    	System.out.println("This is not a valid file");
+		    }
+		    
+    	}
 	    FileReader fr = new FileReader(f);
 	    BufferedReader reader = new BufferedReader(fr);
 	    
@@ -146,6 +180,8 @@ public class CardGame {
 	    //checks to see if the pack is the correct length
 	    if(Cards.size() != numberOfPlayers *8) {
 	    	System.out.println("Incorrect Pack Length");
+	    	myScan2.close();
+	    	reader.close();
 	    	packReader(numberOfPlayers);
 	    }
 	    
